a095c7f73e278c09b691fba9bb6e85d9b937f9b4
Arsenii Iurkov <denver9321@gmail.com>
2022-08-11 22:49:31 +0300

    add styles

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+//= require jquery
+//= require bootstrap-sprockets
+import "bootstrap"
+import jquery from 'jquery'
+window.$ = jquery
+
+import * as bootstrap from 'bootstrap'
+window.bootstrap = bootstrap
