f82ec009acac11a1099d7376b2988ba24b75c732
Arsenii Iurkov <denver9321@gmail.com>
2022-08-19 15:23:18 +0300

    delete seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,1 @@
 # frozen_string_literal: true
-
-i = 0
-3.times do
-  Author.create({ name: FFaker::Book.author })
-end
-
-5.times do
-  Book.create({ title: FFaker::Book.title,
-                description: FFaker::Book.description,
-                price: FFaker::Number.number })
-end
-
-5.times do
-  AuthorBook.create({ author_id: rand(1..3), book_id: i += 1 })
-end
