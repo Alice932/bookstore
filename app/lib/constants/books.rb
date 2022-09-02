204294443ab1110b25990bbe30ba6fb5e6bab7e2
Arsenii Iurkov <denver9321@gmail.com>
2022-09-02 19:08:57 +0300

    add to constnants

--- app/lib/constants/books.rb
+++ app/lib/constants/books.rb
@@ -2,6 +2,8 @@
 
 module Constants
   module Books
+    TRUNCATED_LENGTH = { all_authors: 40,
+                         title: 25 }.freeze
     BOOKS_PER_PAGE = 12
     BOOK_DEFAULT_FILTER = :created_at_desc
     BOOK_FILTERS = {
