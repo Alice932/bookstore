c597217c09659ed624106b3bcf4c8ba87e97548d
Arsenii Iurkov <denver9321@gmail.com>
2022-10-07 16:24:20 +0300

    Edit shrine config

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -2,12 +2,20 @@
 
 require 'shrine'
 require 'shrine/storage/file_system'
-
-Shrine.storages = {
-  cache: Shrine::Storage::FileSystem.new('public', prefix: 'uploads/cache'), # temporary
-  store: Shrine::Storage::FileSystem.new('public', prefix: 'uploads') # permanent
-}
+require 'shrine/storage/s3'
 
 Shrine.plugin :activerecord           # loads Active Record integration
 Shrine.plugin :cached_attachment_data # enables retaining cached file across form redisplays
 Shrine.plugin :restore_cached_data
+
+s3_options = {
+  bucket: 'book-photos',
+  access_key_id: Rails.application.credentials.aws.access_key_id,
+  secret_access_key: Rails.application.credentials.aws.secret_access_key,
+  region: 'us-west-1'
+}
+
+Shrine.storages = {
+  cache: Shrine::Storage::S3.new(prefix: 'cache', **s3_options),
+  store: Shrine::Storage::S3.new(**s3_options)
+}
